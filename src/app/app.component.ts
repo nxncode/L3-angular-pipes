5f2d8c881754ca896b5c3f1fd3d0fd24493c860b
Nixon Augustine <nixon_augustine@hotmail.com>
2020-05-13 00:52:30 +0530

    Angular Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes-n-two-way-binding';
+  title = 'Pipes and Two Way Binding';
+
+  intro : string = 'This tutorial is a tutorial on  Angular';
+  trainer : string = 'Nixon';
+
+  date = new Date();
+  percent : number = 0.52;
+  amount : number = 500;
+  num : number = 256.32154875556;
+  months = ['Jan', 'Feb', 'Mar', 'April', 'May', 'Jun','July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']; 
 }
